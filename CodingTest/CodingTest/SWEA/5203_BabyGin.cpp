e0329a3b09537aa4dd3b1ae66336e154ec21a476
SooJin <jeniffer4669@naver.com>
2026-06-23 14:38:29 +0900

    0623 코테 시작
    개념 문제 정리 및 다시 상기 시키기

---
+++ CodingTest/CodingTest/SWEA/5203_BabyGin.cpp
@@ -0,0 +1,50 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int counts[10] = { 0 };
+	int isTriplet = 0;
+	int isRun = 0;
+
+
+	for (int i = 0; i < 6; i++)
+	{
+		int num = 0;
+		cin >> num;
+		counts[num]++;
+	}
+	//triplete 확인
+	for (int i = 0; i < 10; i++)
+	{
+		if (counts[i] >= 3)
+		{
+			isTriplet++;
+			counts[i] = counts[i] - 3;
+		}
+	}
+
+	//run확인
+	for (int i = 0; i < 10; i++)
+	{
+		if (i + 2 <= 9) {
+			if (counts[i] >= 1 && counts[i + 1] >= 1 && counts[i + 2] >= 1)
+			{
+				isRun++;
+				counts[i]--;
+				counts[i + 1]--;
+				counts[i + 2]--;
+			}
+		}
+	}
+
+	//r1+t1, r2, t2
+	if (isRun + isTriplet >= 2)
+	{
+		cout << "Baby-Gin";
+	}
+	else
+	{
+		cout << "Not Baby-Gin";
+	}
+}
---
+++ CodingTest/CodingTest/SWEA/STL_List.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <list>
+using namespace std;
+
+int main()
+{
+	list<int> myList;
+	list<int>::iterator it;
+
+	for (int i = 0; i < 5; i++)
+	{
+		int num = 0;
+		cin >> num;
+		myList.push_back(num);
+	}
+
+	cout << "리스트 값 전체 출력하기 (노드, 이터레이터)" << endl;
+	for (it = myList.begin(); it != myList.end(); it++)
+	{
+		cout << *it << " ";
+	}
+}
