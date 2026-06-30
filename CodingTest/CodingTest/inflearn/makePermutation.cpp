846867959e9fba39ecfb05897bc7bdab0bc5b8b0
SooJin <jeniffer4669@naver.com>
2026-06-30 20:46:48 +0900

    순열 만들기

---
+++ CodingTest/CodingTest/inflearn/makePermutation.cpp
@@ -0,0 +1,46 @@
+﻿/*
+* 순열은 기본적으로 swap을 통해 생성된다.
+* 반드시 도식화 해보고 makePermutation 함수 구현해보기
+*/
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+vector<int> v;
+
+void PrintV(vector<int>& v)
+{
+	for (int i = 0; i < v.size(); i++)
+	{
+		cout << v[i] << " ";
+	}
+	cout << endl;
+}
+
+void makePermutation(int n, int r, int depth)
+{
+	cout << n << " : " << r << " : " << depth << endl;
+	
+	if (r == depth) 
+	{
+		PrintV(v);
+		return;
+	}
+	for (int i = depth; i < n; i++)
+	{
+		swap(v[i], v[depth]);
+		makePermutation(n, r, depth + 1);
+		swap(v[i], v[depth]);
+	}
+	return;
+}
+
+int main()
+{
+	for (int i = 1; i <= 3; i++)
+		v.push_back(i);
+
+	makePermutation(3, 3, 0);
+	return 0;
+}
