3d1a67f7a4281cfabd8e2f280e876c11ec2b89b5
SooJin <jeniffer4669@naver.com>
2026-07-14 15:44:27 +0900

    TC4

---
+++ CodingTest/CodingTest/inflearn/TimeComplexity4.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+using namespace std;
+
+int N, cnt;
+void solve(int N) {
+	cnt++;	//main logic  =>   O(1)
+	cout << "cnt : " << cnt << endl;
+	if (N == 0) return;
+	for (int i = 0; i < 3; i++)
+	{
+		solve(N - 1); //이 함수가 몇번 실행?  =>   O(3^n)
+	}
+	return;
+}
+//O(1) x O(3^n) = O(3^n)
+int main()
+{
+	cin >> N;
+	solve(N);
+	return 0;
+}
+//https://dooding.tistory.com/75
