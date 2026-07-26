8aca01d4fcef835ac32971d3832a08a9e5fa66e7
SooJin <jeniffer4669@naver.com>
2026-07-26 17:08:51 +0900

    1E

---
+++ CodingTest/CodingTest/inflearn/cosal/1D.cpp
@@ -0,0 +1,52 @@
+﻿#include <iostream>
+#include <algorithm> //reverse는 포함해야 함.
+using namespace std;
+string word;
+string temp;
+
+int main()
+{
+	cin >> word;
+	temp = word;
+	reverse(word.begin(), word.end()); //원본 배열에 영향이 감.
+	if (word == temp) cout << "1";
+	
+	else
+	{
+		cout << "0";
+	}
+
+	return 0;
+}
+
+
+
+/*
+* 내가 푼 문제
+string word;
+int l, r;
+
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	cin >> word;
+
+	l = 0;
+	r = word.size() - 1;
+
+	for (l = 0; l != r; l++, r--)
+	{
+		if (word[l] != word[r]) {
+			cout << "0";
+			break;
+		}
+	}
+
+	if (l == r)
+		cout << "1";
+
+	return 0;
+}*/
---
+++ CodingTest/CodingTest/inflearn/cosal/1E.cpp
@@ -0,0 +1,36 @@
+﻿#include <bits/stdc++.h>
+using namespace std;
+int name[30] = { 0 };
+bool isGame = false;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	int count = 0;
+	cin >> count;
+
+	for (int i = 0; i < count; i++)
+	{
+		string c;
+		cin >> c;
+		name[c[0] - 'a']++;
+	}
+
+	for (int i = 0; i < 30; i++)
+	{
+		if (name[i] >= 5)
+		{
+			cout << (char)(i + 'a');
+			isGame = true;
+		}
+
+	}
+	if (!isGame)
+	{
+		cout << "PREDAJA";
+	}
+	
+	return 0;
+}
