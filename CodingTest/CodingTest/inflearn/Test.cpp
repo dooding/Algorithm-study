095ceecd5aa3f50677172c47abe39b08494667e2
SooJin <jeniffer4669@naver.com>
2026-07-26 13:28:10 +0900

    문제 풀이

---
+++ CodingTest/CodingTest/inflearn/Test.cpp
@@ -0,0 +1,161 @@
+﻿#include <iostream>
+#include <string>
+#include <iomanip>
+using namespace std;
+
+//36페이지 1.4 타입과 타입 변환 부터 하기
+
+/*
+* 
+int a = 1;
+char s = 'a';
+string str = "어벤져스";
+double d = 1.223123;
+
+int main()
+{
+	printf("아이엠어 아이언맨 : %d\n", a);
+	printf("아이엠어 아이언맨 : %c\n", s);
+	printf("아이엠어 아이언맨 : %s\n", str.c_str());
+	printf("아이엠어 아이언맨 : %lf\n", d);
+}
+
+
+
+/*
+typedef long long ll;
+double d = 1.23456789;
+int e = 12;
+int f = 2;
+
+int a = 1, b = 2;
+int main()
+{
+	printf("홍철 %d : 지수 %d\n", a, b);
+
+	printf("%.6lf\n", d);
+	printf("%02d\n", e);
+	printf("%02d\n", f);
+
+	return 0;
+}
+
+//typedef long long ll;
+double a = 1.23456789;
+
+int main()
+{
+	cout << a << "\n";
+	cout << fixed << setprecision(3) << a << "\n";
+	cout << fixed << setprecision(8) << a << "\n";
+	return 0;
+}
+
+
+
+* cin 이후에 버퍼 비우기
+int T;
+string s;
+int main()
+{
+	cin >> T;
+	string bufferFlush;
+	getline(cin, bufferFlush);
+
+	for (int i = 0; i < T; i++)
+	{
+		getline(cin, s);
+		cout << s << "\n";
+	}
+	return 0;
+}
+
+
+
+string s, d;
+int main()
+{
+	getline(cin, s);
+	cout << s << "\n";
+
+	getline(cin, d, '.');
+	cout << d << '\n';
+	return 0;
+
+
+}
+
+* 실수 = 정수.정수
+#define _CRT_SECURE_NO_WARNINGS  // 반드시 #include보다 위에!
+
+int a, b;
+double c;
+int main()
+{
+	scanf("%d.%d", &a, &b);
+	printf("\n%d %d\n", a, b);
+
+	scanf("%lf", &c);
+	printf("%lf\n", c);
+	return 0;
+}
+
+
+* scanf 사용법
+#define _CRT_SECURE_NO_WARNINGS  // 반드시 #include보다 위에!
+
+int a ;
+double b ;
+char c ;
+
+int main()
+{
+	//scanf("%d %lf %c", &a, &b, &c);
+	int rey = scanf("%d %lf %c", &a, &b, &c);
+	printf("성공개수 : %d\n", rey);
+	printf("%d\n", a);
+	printf("%lf\n", b);
+	printf("%c\n", c);
+
+	return 0;
+}
+
+* cin 입력 (띄어쓰기하면 그 다음껀 그 다음이 받음)
+string a, b;
+
+int main()
+{
+	cin >> a >> b;
+	cout << a << "\n";
+	cout << b << "\n";
+	return 0;
+}
+
+
+
+* define 방법
+#define PI 3.14159
+#define loop(i,n) for(int i = 0; i < n; i ++)
+
+int main()
+{
+	cout << PI << "\n";
+	int sum = 0;
+	loop(i, 10)
+	{
+		sum += i;
+	}
+	cout << sum << "\n";
+	return 0;
+}
+
+
+* 출력
+string a;
+int main()
+{
+	cin >> a;
+	cout << a << "\n";
+	return 0;
+}
+*/
---
+++ CodingTest/CodingTest/inflearn/cosal/1A.cpp
@@ -0,0 +1,163 @@
+﻿/*
+순열_재귀함수
+*/
+#include <iostream>
+#include <algorithm>
+using namespace std;
+int a[9] = { 0 };
+
+void print()
+{
+	for (int i = 0; i < 7; i++)
+	{
+		cout << a[i] << " ";
+	}
+	cout << "\n";
+}
+
+void makePermutation(int n, int r, int depth)
+{
+	if (r == depth)
+	{
+		//logic
+		print();
+		return;
+	}
+	for (int i = depth; i < n; i++)
+	{
+		swap(a[i], a[depth]);
+		makePermutation(n, r, depth + 1);
+		swap(a[i], a[depth]);
+	}
+}
+int main()
+{
+	for (int i = 0; i < 9; i++)
+	{
+		cin >> a[i];
+	}
+
+	int n = 9;
+	int r = 7;
+	makePermutation(n, r, 0);
+	
+}
+/*
+* 순열로 7개 뽑기
+do{
+}while(next_permutation(start, end);
+
+
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int a[9] = { 0 };
+
+int main()
+{
+	for (int i = 0; i < 9; i++)
+	{
+		cin >> a[i];
+	}
+
+	do
+	{
+		int sum = 0;
+		for (int i = 0; i < 7; i++)
+		{
+			sum += a[i];
+		}
+		if (sum == 100)
+			break;
+
+	} while (next_permutation(a, a+9));
+
+	sort(a, a + 7);
+	for (int i = 0; i < 7; i++) cout << a[i] << " ";
+	return 0;
+
+}
+*/
+
+/*
+1-A 23009 일곱 난쟁이 (조합_재귀함수 방식으로 풀었음)
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+int n = 9;
+int k = 7;
+vector<int> tall;
+
+void print(vector<int> v)
+{
+	for (int i: v)
+	{
+		cout << tall[i] << " ";
+	}
+	cout << "\n";
+}
+
+bool sum(vector<int> v)
+{
+	int sum = 0;
+	for (int i : v)
+	{
+		sum += tall[i];
+	}
+	return sum == 100;
+}
+
+void combi(int start, vector<int>& v)
+{
+	if (v.size() == k)
+	{
+		if (sum(v))
+		{
+			print(v);
+		}
+		return;
+	}
+	for (int i = start + 1; i < n; i++)
+	{
+		v.push_back(i);
+		combi(i, v);
+		v.pop_back();
+	}
+}
+
+int main()
+{
+	//combi 실행
+
+	vector<int> t;
+
+	for (int i = 0; i < 9; i++)
+	{
+		int temp;
+		cin >> temp;
+		tall.push_back(temp);
+	}
+	sort(tall.begin(), tall.end());
+	combi(-1, t);
+}
+*/
+/*
+* 조합 함수 틀
+void combi(int start, vector<int>& b)
+{
+	if (k == b.size())
+	{
+		//logic
+		return;
+	}
+	for (int i = start + 1; i < n; i++)
+	{
+		b.push_back(i);
+		combi(i, b);
+		b.pop_back();
+	}
+}
+*/
